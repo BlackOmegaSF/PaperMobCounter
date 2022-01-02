81cf259af0c7f35280ae3a88adb74a568e5168b5
BlackOmegaSF <kian.leiner@gmail.com>
2022-01-01 18:06:50 -0800

    Initial commit

---
+++ src/main/java/com/floogoobooq/blackomega/papermobcounter/PaperMobCounter.java
@@ -0,0 +1,428 @@
+package com.floogoobooq.blackomega.papermobcounter;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PaperMobCounter extends JavaPlugin implements Listener {
+
+    private HashMap<UUID, Scoreboard> customBoards = new HashMap<UUID, Scoreboard>();
+    private HashMap<UUID, HashSet<EntityType>> entityTracker = new HashMap<UUID, HashSet<EntityType>>();
+    private ScoreboardManager sbm;
+    private HashSet<UUID> playersToScoreboard = new HashSet<UUID>();
+
+    File saveFile;
+    FileConfiguration saveData;
+
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this,this);
+        sbm = Bukkit.getScoreboardManager();
+
+        if (!(saveFile = new File(getDataFolder(), "mobcounts.yml")).exists()) {
+            saveFile = new File(getDataFolder(), "mobcounts.yml");
+        }
+        saveData = YamlConfiguration.loadConfiguration(saveFile);
+
+        for (Map.Entry<String, Object> entry : saveData.getValues(true).entrySet()) {
+            String playerId;
+            UUID playerUuid;
+            String entityName;
+            //getLogger().log(Level.INFO, "Processing YAML Entry: " + entry.getKey());
+
+            try {
+                playerId = entry.getKey().split("\\.")[0];
+                playerUuid = UUID.fromString(playerId);
+                entityName = entry.getKey().split("\\.")[1];
+            } catch (IndexOutOfBoundsException e) {
+                getLogger().log(Level.INFO, "Loading player scoreboard: " + entry.getKey().split("\\.")[0]);
+                continue;
+            }
+
+            Scoreboard board;
+            Objective obj;
+            if (customBoards.containsKey(playerUuid)) {
+                board = customBoards.get(playerUuid);
+                obj = board.getObjective("Kill Counter");
+            } else {
+                board = sbm.getNewScoreboard();
+                obj = board.registerNewObjective("Kill Counter", "dummy", "Mob Kill Counter");
+                obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+                customBoards.put(playerUuid, board);
+            }
+
+            obj.getScore(entityName).setScore((int)entry.getValue());
+            HashSet<EntityType> etList = entityTracker.get(playerUuid);
+            if (etList == null) {
+                etList = new HashSet<EntityType>();
+            }
+            etList.add(EntityType.valueOf(entityName));
+            entityTracker.put(playerUuid, etList);
+
+        }
+
+        for (Map.Entry<UUID, Scoreboard> entry : customBoards.entrySet()) {
+            Player player = Bukkit.getPlayer(entry.getKey());
+            if (player != null) {
+                //player is online
+                player.setScoreboard(entry.getValue());
+            } else {
+                //player is offline
+                playersToScoreboard.add(entry.getKey());
+            }
+        }
+
+
+        //Register the command
+        this.getCommand("countmobs").setExecutor(new CommandHandler());
+        this.getCommand("countmobs").setTabCompleter(new EntityTabCompletion());
+
+    }
+
+    @Override
+    public void onDisable() {
+        //Save data to saveFile
+        for (Map.Entry<UUID, HashSet<EntityType>> entry : entityTracker.entrySet()) {
+            for(EntityType entityEntry : entry.getValue()) {
+                int score = customBoards.get(entry.getKey()).getObjective("Kill Counter").getScore(entityEntry.toString()).getScore();
+                saveData.set(entry.getKey().toString() + "." + entityEntry.toString(), score);
+            }
+        }
+
+        try {
+            saveData.save(saveFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @EventHandler
+    public void onPlayerJoin(final PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        UUID playerId = player.getUniqueId();
+
+        if (playersToScoreboard.contains(playerId)) {
+            player.setScoreboard(customBoards.get(playerId));
+            playersToScoreboard.remove(playerId);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(final PlayerQuitEvent event) {
+        UUID playerId = event.getPlayer().getUniqueId();
+
+        if (customBoards.containsKey(playerId)) {
+            playersToScoreboard.add(playerId);
+        }
+    }
+
+
+    //Command responder class
+    public class CommandHandler implements CommandExecutor {
+
+        final private String _START = "START";
+        final private String _RESUME = "RESUME";
+        final private String _STOP = "STOP";
+        final private String _GETCOUNT = "GETCOUNT";
+        final private String _RESET = "RESET";
+
+        @Override
+        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (args.length == 0 || args[0].equalsIgnoreCase("help")) {
+                    return false;
+                }
+
+                EntityType toCount;
+                try {
+                    toCount = EntityType.valueOf(args[1].toUpperCase());
+
+                } catch (IllegalArgumentException|NullPointerException|IndexOutOfBoundsException e) {
+                    sender.sendMessage("Invalid mob");
+                    return false;
+                }
+                String entityName = toCount.toString();
+                //String entityShortName = entityName.substring(0, Math.min(entityName.length(), 16));  //Trimmed to 16 to avoid error
+
+                switch (args[0].toUpperCase()) {
+                    case _START:
+                        //TODO Handle start condition
+                        try {
+                            //int countLimit;
+                            //boolean scoreboardVisible;
+
+                            /*
+                            try {
+                                countLimit = Integer.parseInt(args[2]);
+                                if (countLimit < 0) {
+                                    throw new NumberFormatException();
+                                }
+                            } catch (NumberFormatException e) {
+                                sender.sendMessage("Invalid count");
+                            }
+                            */
+
+                            //scoreboardVisible = Boolean.parseBoolean(args[3]);
+
+                            //Get or create scoreboard for player
+                            Scoreboard board = customBoards.get(player.getUniqueId());
+                            if (board == null) {
+                                board = sbm.getNewScoreboard();
+                                customBoards.put(player.getUniqueId(), board);
+                            }
+                            player.setScoreboard(board);
+
+                            //Register new objective in scoreboard using info here
+                            Objective obj = board.getObjective("Kill Counter");
+                            if (obj == null) {
+                                obj = board.registerNewObjective("Kill Counter", "dummy", "Mob Kill Counter");
+                                obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+                            }
+
+
+
+                            //Add entity to entity tracker
+                            HashSet<EntityType> etList = entityTracker.get(player.getUniqueId());
+                            if (etList == null) {
+                                etList = new HashSet<EntityType>();
+                            }
+                            if (etList.contains(toCount)) {
+                                sender.sendMessage("Already counting " + toCount.toString());
+                                return true;
+                            }
+
+                            etList.add(toCount);
+                            entityTracker.put(player.getUniqueId(), etList);
+
+                            //Set up score entry
+                            obj.getScore(entityName).setScore(0);
+
+                            player.setScoreboard(board);
+
+                            return true;
+
+                        } catch (IndexOutOfBoundsException e) {
+                            //TODO Handle the exception with a message
+                            sender.sendMessage("Too few arguments");
+                            return false;
+                        }
+
+                    /*case _RESUME:
+                        //TODO Handle resume condition
+
+
+                        break;
+                     */
+
+                    case _STOP:
+                        //TODO Handle stop condition
+                        try {
+                            Scoreboard board = customBoards.get(player.getUniqueId());
+                            if (board == null) {
+                                sender.sendMessage("No scoreboard initialized for " + player.getDisplayName() + ". Make sure to start a count before stopping.");
+                                return true;
+                            }
+
+                            Objective obj = board.getObjective("Kill Counter");
+                            if (obj != null) {
+
+                                HashSet<EntityType> etList = entityTracker.get(player.getUniqueId());
+                                if (etList == null || !etList.contains(toCount)) {
+                                    sender.sendMessage("Not currently counting " + entityName);
+                                    return true;
+                                }
+
+                                board.resetScores(entityName);
+
+                                etList.remove(toCount);
+                                if (etList.size() == 0) {
+                                    obj.unregister();
+                                    player.setScoreboard(sbm.getMainScoreboard());
+                                    customBoards.remove(player.getUniqueId());
+                                    entityTracker.remove(player.getUniqueId());
+                                    saveData.set(player.getUniqueId().toString(), null);
+                                    return true;
+                                }
+                                saveData.set(player.getUniqueId().toString() + "." + entityName, null);
+                                entityTracker.put(player.getUniqueId(), etList);
+
+                            } else {
+                                sender.sendMessage("Objective not initialized.");
+                            }
+
+                            return true;
+
+
+                        } catch (NullPointerException e) {
+                            sender.sendMessage("Welp, this ABSOLUTELY shouldn't happen. Tell Kian IMMEDIATELY because it's bad.");
+                            return true;
+                        }
+
+
+                    /*case _GETCOUNT:
+                        //TODO Handle getcount condition
+                        break;
+                     */
+
+                    case _RESET:
+                        //TODO Handle reset condition
+                        try {
+                            Scoreboard board = customBoards.get(player.getUniqueId());
+                            if (board == null) {
+                                sender.sendMessage("No scoreboard initialized for " + player.getDisplayName() + ". Make sure to start a count before stopping.");
+                                return true;
+                            }
+
+                            Objective obj = board.getObjective("Kill Counter");
+                            if (obj != null) {
+
+                                HashSet<EntityType> etList = entityTracker.get(player.getUniqueId());
+                                if (etList == null || !etList.contains(toCount)) {
+                                    sender.sendMessage("Not currently counting " + entityName);
+                                    return true;
+                                }
+
+                                Score score = obj.getScore(entityName);
+                                score.setScore(0);
+
+                            } else {
+                                sender.sendMessage("Objective not initialized.");
+                            }
+
+                            return true;
+
+                        } catch (NullPointerException e) {
+                            sender.sendMessage("Welp, this ABSOLUTELY shouldn't happen. Tell Kian IMMEDIATELY because it's bad.");
+                            return true;
+                        }
+
+
+                    default:
+                        sender.sendMessage("Invalid command");
+                        return false;
+                }
+
+            } else {
+                sender.sendMessage("Only players can use this command.");
+                return false;
+            }
+        }
+
+    }
+
+    @EventHandler
+    public void onEntityDeath(final EntityDeathEvent event) {
+        LivingEntity killedMob = event.getEntity();
+        Player killer = killedMob.getKiller();
+
+        if (killer != null) {
+
+            EntityType killedType = killedMob.getType();
+            HashSet<EntityType> etList = entityTracker.get(killer.getUniqueId());
+            if (etList != null && etList.contains(killedType)) {
+                Scoreboard board = customBoards.get(killer.getUniqueId());
+                if (board != null) {
+                    Objective obj = board.getObjective("Kill Counter");
+                    if (obj != null) {
+                        Score score = obj.getScore(killedType.toString());
+                        score.setScore(score.getScore() + 1);
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    public class EntityTabCompletion implements TabCompleter {
+
+        @Override
+        public List<String> onTabComplete(CommandSender commandSender, Command command, String alias, String[] args) {
+
+            List<String> names = new ArrayList<String>();
+            if (args.length == 1) {
+                if (args[0].isEmpty()) {
+                    names.add("start");
+                    names.add("reset");
+                    names.add("stop");
+                } else {
+                    Pattern pattern = Pattern.compile(args[0].toLowerCase());
+                    if (pattern.matcher("start").lookingAt()) {
+                        names.add("start");
+                    }
+                    if (pattern.matcher("reset").lookingAt()) {
+                        names.add("reset");
+                    }
+                    if (pattern.matcher("stop").lookingAt()) {
+                        names.add("stop");
+                    }
+                }
+
+                return names;
+
+            } else if (args.length == 2) {
+                if (args[0].equals("start")) {
+                    for (EntityType entry : EntityType.values()) {
+                        names.add(entry.toString());
+                    }
+                } else if (args[0].equals("reset") || args[0].equals("stop")) {
+                    if (commandSender instanceof Player && entityTracker.containsKey(((Player) commandSender).getUniqueId())) {
+                        HashSet<EntityType> etList = entityTracker.get(((Player) commandSender).getUniqueId());
+                        if (!etList.isEmpty()) {
+                            for (EntityType entry : etList) {
+                                names.add(entry.toString());
+                            }
+                        }
+                    }
+                } else {
+                    return null;
+                }
+
+                List<String> filteredNames = new ArrayList<String>();
+                if (!args[1].isEmpty()) {
+                    Pattern pattern = Pattern.compile(args[1].toUpperCase());
+                    for (String name : names) {
+                        if (pattern.matcher(name).lookingAt()) {
+                            filteredNames.add(name);
+                        }
+                    }
+                    return filteredNames;
+                }
+
+                return names;
+            }
+
+
+
+
+            return names;
+        }
+    }
+
+
+
+}
