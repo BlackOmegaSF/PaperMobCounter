59635500c54156cb92fec03608b2b5cca172c358
BlackOmegaSF <kian.leiner@gmail.com>
2022-01-02 08:50:35 -0800

    Optimize imports

--- src/main/java/com/floogoobooq/blackomega/papermobcounter/PaperMobCounter.java
+++ src/main/java/com/floogoobooq/blackomega/papermobcounter/PaperMobCounter.java
@@ -25,7 +25,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 import java.util.logging.Level;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PaperMobCounter extends JavaPlugin implements Listener {
